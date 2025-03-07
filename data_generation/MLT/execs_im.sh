7c568bb39ae65660b4d032e5c88d13f4c9ed6504
UltimateJupiter <Jupiter.zhuxingyu@gmail.com>
2025-03-07 14:14:11 -0500

    [init commit] Misc items for checkpointing / visualization

--- data_generation/MLT/execs_im.sh
+++ data_generation/MLT/execs_im.sh
@@ -13,7 +13,7 @@ seed=0
 
 job_name="dict_gen_${attr}_C${nchars}_ND${n_dictionaries}_NS${n_samples_per_dictionary}_D${depth}_S${seed}_HO${ho_rate}"
 
-sbatch -J $job_name -c 32 --partition=pli-c --time=1:00:00 --mem=256GB -o ./logs/${job_name}.out dict_gen.sh $attr $nchars $n_dictionaries $n_samples_per_dictionary $depth $seed $additional_args $ho_rate
+bash dict_gen.sh $attr $nchars $n_dictionaries $n_samples_per_dictionary $depth $seed $additional_args $ho_rate
 
 done
 done
